0bb1777edb1064561393cd92452435f40d84b3ab
Peter Tardif <peter.tardif@gmail.com>
2024-06-27 22:47:02 -0600

    Added fonts, images, and sidenav links

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,11 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
-export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '700'],
+});
